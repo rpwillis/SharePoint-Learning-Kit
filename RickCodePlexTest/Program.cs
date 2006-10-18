ddc67a7e897b8dfdcef1802eec5497bca8416c75
SND\EricLedoux_cp <SND\EricLedoux_cp@localhost>
2006-10-18 16:23:44 +0000

    This is a CodePlex test

---
+++ RickCodePlexTest/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RickCodePlexTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
+// This is a placeholder.
