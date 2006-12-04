6ffd7665afa36e8e81f2d2412dbd598236101e20
SND\SLKBuilder_cp <SND\SLKBuilder_cp@localhost>
2006-12-04 10:00:55 +0000

    Build 1.0.780

--- Src/Shared/FileVersion.cs
+++ Src/Shared/FileVersion.cs
@@ -1,1 +1,1 @@
-[assembly: System.Reflection.AssemblyFileVersion("1.0.779.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.780.0")]
--- Src/Shared/Version.cs
+++ Src/Shared/Version.cs
@@ -1,1 +1,1 @@
-[assembly: System.Reflection.AssemblyVersion("1.0.779.0")]
+[assembly: System.Reflection.AssemblyVersion("1.0.780.0")]
