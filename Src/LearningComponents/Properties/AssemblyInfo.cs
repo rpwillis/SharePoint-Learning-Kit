b88caacac7c52395ae76de7c95ac39e4f46254fa
SND\VandanaPonnuru_cp <SND\VandanaPonnuru_cp@localhost>
2008-01-09 12:27:07 +0000

    - Language Pack Version changed to 1.1.0.0 - Satellite Contract Version on main assemblies set to 1.1.0.0
    Language pack version changes. The difference in this new version is the change in the keypair used to sign these assemblies.
    While releases, 1.0, 1.1, 1.2 worked with the language pack versions 1.0.799.0, releases after 1.3.0 will work with the 1.1.0.0 language packs.

--- Src/LearningComponents/Properties/AssemblyInfo.cs
+++ Src/LearningComponents/Properties/AssemblyInfo.cs
@@ -17,3 +17,5 @@ using System;
 [assembly: InternalsVisibleTo("Microsoft.LearningComponents.SharePoint, PublicKey=00240000048000009400000006020000002400005253413100040000010001009f69f9c3535f65a7e8684cdc8237ff3e976289ca5715eb3e722c526bcb22cb0cdecfeff8e91251a9eb349b35342fc64aff2de04e8e1ea9554654b4f5871103da9d090c7b0ed45b1d6b1e2e1dc281274f6b3c25890d7d0aee41fcc0fa911675098afb2373a9f50b212dd154506402237bfef8b4297e2ed2dea771193acd4524b4")]
 
 [assembly: CLSCompliant(true)]
+
+[assembly: System.Resources.SatelliteContractVersion("1.1.0.0")]
