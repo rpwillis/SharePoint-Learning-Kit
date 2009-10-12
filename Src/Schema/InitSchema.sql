195004f91f43c9b7de6fdb067d0d750f0a7c808f
RichardWillis <rpwillis@users.noreply.github.com>
2009-10-12 22:43:57 +0000

    Allow larger suspend data size for SCORM v3.

--- Src/Schema/InitSchema.sql
+++ Src/Schema/InitSchema.sql
@@ -1240,7 +1240,7 @@ CREATE TABLE [ActivityAttemptItem](
     [SessionStartTimestamp] datetime DEFAULT NULL,
     [SessionTime] float(53) DEFAULT NULL,
     [SuccessStatus] int DEFAULT NULL,
-    [SuspendData] nvarchar(max) DEFAULT NULL CHECK(LEN([SuspendData])<=4096),
+    [SuspendData] nvarchar(max) DEFAULT NULL CHECK(LEN([SuspendData])<=64000),
     [TotalTime] float(53) DEFAULT 0
 )
 GRANT SELECT, INSERT, DELETE, UPDATE ON [ActivityAttemptItem] TO LearningStore
