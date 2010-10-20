ae59b08ceab223cbfbc645265e4f54cc02cc99af
Richard Willis <rpwillis@users.noreply.github.com>
2010-10-20 09:49:47 +0100

    Removed MRCI project.

--- GAC.bat
+++ GAC.bat
@@ -1,14 +1,12 @@
 @echo off
 rem  Copyright (c) Microsoft Corporation. All rights reserved. 
 rem -- installs MLC and SLK DLLs into the Global Assembly Cache
-gacutil /nologo /i Src\Compression\MRCI\bin\Debug\Microsoft.LearningComponents.MRCI.dll
 gacutil /nologo /i Src\Compression\Compression\bin\Debug\Microsoft.LearningComponents.Compression.dll
 gacutil /nologo /i Src\LearningComponents\bin\Debug\Microsoft.LearningComponents.dll
 gacutil /nologo /i Src\SharePoint\bin\Debug\Microsoft.LearningComponents.SharePoint.dll
 gacutil /nologo /i Src\Storage\bin\Debug\Microsoft.LearningComponents.Storage.dll
 gacutil /nologo /i Slk\Dll\bin\Debug\Microsoft.SharePointLearningKit.dll
 echo List of MLC and SLK DLLs in the Global Assembly Cache:
-gacutil /nologo /l Microsoft.LearningComponents.MRCI | findstr /c:Version=
 gacutil /nologo /l Microsoft.LearningComponents.Compression | findstr /c:Version=
 gacutil /nologo /l Microsoft.LearningComponents | findstr /c:Version=
 gacutil /nologo /l Microsoft.LearningComponents.SharePoint | findstr /c:Version=
--- Samples/BasicWebPlayer/Init.bat
+++ Samples/BasicWebPlayer/Init.bat
@@ -2,8 +2,6 @@ rem  Copyright (c) Microsoft Corporation. All rights reserved.
 
 rem -- see ReadMe.txt
 mkdir Bin
-copy /y ..\..\Debug\Microsoft.LearningComponents.MRCI.dll Bin
-copy /y ..\..\Debug\Microsoft.LearningComponents.MRCI.pdb Bin
 copy /y ..\..\Debug\Microsoft.LearningComponents.Compression.dll Bin
 copy /y ..\..\Debug\Microsoft.LearningComponents.Compression.pdb Bin
 copy /y ..\..\Debug\Microsoft.LearningComponents.dll Bin
