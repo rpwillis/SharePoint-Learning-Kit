72c741709e55b0f14dd5a286032e82afbd51bcdf
Richard Willis <rpwillis@users.noreply.github.com>
2010-10-20 10:36:34 +0100

    Tidied top level folder a bit.

--- Prop.bat
+++
@@ -1,8 +0,0 @@
-rem  Copyright (c) Microsoft Corporation. All rights reserved. 
-
-rem -- Propagates MLC and SLK code to SharePoint
-call UnGAC.bat
-call GAC.bat noiisreset
-pushd Slk
-nmake prop
-popd
