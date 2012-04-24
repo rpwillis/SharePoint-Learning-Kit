03a54b7bce7dee0d86de4c6642826f6f22828e2e
Richard Willis <rpwillis@users.noreply.github.com>
2012-04-25 00:21:15 +0100

    Added more documentation to DomainGroupEnumerator.

--- Slk/Dll/enumerateDomainGroups/domainGroupEnumerator.cs
+++ Slk/Dll/enumerateDomainGroups/domainGroupEnumerator.cs
@@ -15,6 +15,11 @@ namespace Microsoft.SharePointLearningKit
 
 #region public methods
         /// <summary>Enumerates a domain group.</summary>
+        /// <param name="domainGroup">The domain group to enumerate.</param>
+        /// <param name="web">The SPWeb the permission is for. This may be needed to add users to SharePoint if they aren't already added. Used by the default implementation as not all group members may have accessed SharePoint before. May not be needed for other implementation.</param>
+        /// <param name="timeRemaining">The time remaining to enumerate all groups. This can be ignored as SLK will check it before enumerating any more groups if there are any. Used by the default implementation to check time elapsed before enumerating nested groups.</param>
+        /// <param name="hideDisabledUsers">Whether to hide disabled members of the group. Primarily used by the default implementation.</param>
+        /// <returns></returns>
         public abstract DomainGroupEnumeratorResults EnumerateGroup(SPUser domainGroup, SPWeb web, TimeSpan timeRemaining, bool hideDisabledUsers);
 #endregion public methods
 
