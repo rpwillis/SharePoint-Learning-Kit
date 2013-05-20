f1969c82db7fb078e4dc38d4f079d471cf33e3f9
Richard Willis <rpwillis@users.noreply.github.com>
2013-05-20 12:28:20 +0100

    Added localized Slk.resx to build process.

---
+++ Slk/Tools/LocalizationGuide/Src/Replacer.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+
+namespace SharePointLearningKit.Localization
+{
+    class Replacer
+    {
+        string culture;
+        string input;
+
+        public Replacer(string culture)
+        {
+            if (string.IsNullOrEmpty(culture))
+            {
+                throw new ArgumentOutOfRangeException("culture is required.");
+            }
+
+            this.culture = culture;
+        }
+
+        public void Load(string source)
+        {
+            input = File.ReadAllText(source);
+        }
+
+        public void Save(string destination)
+        {
+            string output = input.Replace("Culture", culture);
+            File.WriteAllText(destination, output);
+        }
+
+    }
+}
