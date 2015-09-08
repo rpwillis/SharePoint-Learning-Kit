038a8858fdd08f8c6047730eeb626f33ce53127f
RichardWillis <rpwillis@users.noreply.github.com>
2015-09-08 12:05:15 +0100

    Fix build process to handle css in SharePoint 2013.

--- Slk/Dll/Properties/AssemblyInfo.cs
+++ Slk/Dll/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System;
 
 // Version information
 [assembly: System.Reflection.AssemblyVersion("1.3.1.0")]
-[assembly: System.Reflection.AssemblyFileVersion("1.8.1.2")]
+[assembly: System.Reflection.AssemblyFileVersion("1.8.2.0")]
 
 // Other general attributes
 [assembly: AssemblyTitle("Microsoft SharePoint Learning Kit Module")]
